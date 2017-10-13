3e7a3fab84ab9ff240fbc409e772399a370965e4
Joshua Prpich <joshuaprpich@gmail.com>
2017-10-12 20:50:35 -0500

    solucionado tercera prueba

--- challenges/ruby/3-local-variables/my_solution.rb
+++ challenges/ruby/3-local-variables/my_solution.rb
@@ -2,8 +2,14 @@
 
 
 # Tu solucion abajo:
+num1=123
+num2=232
 
-
+sum = num1 + num2
+difference = num1 - num2
+quotient = num1.to_f/num2.to_f
+product = num1*num2
+modulus = num1%num2
 # Pruebas NO LAS BORRES
 
 
