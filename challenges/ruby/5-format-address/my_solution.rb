f7d3f47450417d865bb418e34a8f73bc1a4b2c82
Leonardo Omaña Cortés <leonardoomanacortes@MacBook-Pro-de-Leonardo.local>
2017-10-13 21:03:44 -0500

    Ejercicio 5 Ruby

--- challenges/ruby/5-format-address/my_solution.rb
+++ challenges/ruby/5-format-address/my_solution.rb
@@ -1,7 +1,9 @@
 # Format an Address
 
 # Tu solucion abajo:
-def make_address ("street", "city", "state", "zip")
+def make_address (street, city, state, zip)	
+	 "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
 end
 make_address("633 Folsom St.","San Francisco","CA",94107)
+
 
