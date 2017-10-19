aab81559c9e82a146ded58bd235c5b6f98c1e2aa
Joshua Prpich <joshuaprpich@gmail.com>
2017-10-18 20:51:51 -0500

    solucionado el reto 17

--- challenges/ruby/17-count-between/my_solution.rb
+++ challenges/ruby/17-count-between/my_solution.rb
@@ -1,4 +1,19 @@
 # Count Between
 
 # Tu solucion abajo:
+def count_between(arr,lower,upper)
+  return  0 if arr.empty?
+  arr.reduce(0) do |accu, num|
+    accu += 1 if num >= lower && num <= upper
+    accu
+  end
+end
+
+
+
+
+
+
+
+
 
