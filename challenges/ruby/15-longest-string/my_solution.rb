cd6c848d97f6becf4b2e213f11a39d46369ce829
Joshua Prpich <joshuaprpich@gmail.com>
2017-10-18 20:24:08 -0500

    solucionado ejercicio 15

--- challenges/ruby/15-longest-string/my_solution.rb
+++ challenges/ruby/15-longest-string/my_solution.rb
@@ -2,3 +2,13 @@
 
 # Tu solucion abajo:
 
+def longest_string(arr)
+ return nil if arr.empty?
+ result = arr[0]  
+ arr.each do |value|
+   if value.size > result.size
+     result = value
+   end
+ end
+ result
+end
