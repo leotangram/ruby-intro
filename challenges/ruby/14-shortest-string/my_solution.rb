b3b50381518fdfe3ee73c57ee0fc1281a319efc7
Joshua Prpich <joshuaprpich@gmail.com>
2017-10-18 20:17:20 -0500

    solucion joshua 14

--- challenges/ruby/14-shortest-string/my_solution.rb
+++ challenges/ruby/14-shortest-string/my_solution.rb
@@ -1,6 +1,19 @@
 # Shortest String
 
-Tu solucion abajo:
+# Tu solucion abajo:
+# def shortest_string(arr)
+#   return nil if arr.empty?
+#   min = arr[0].size
+#   result = arr[0]
+#   arr.each do |x|
+#     if x.size < min 
+#       min = x.size
+#       result = x
+#     end
+#   end
+#   result
+# end
+
 def shortest_string(arr)
   return nil if arr.empty?
   min = arr[0].size
@@ -13,8 +26,6 @@ def shortest_string(arr)
   end
   result
 end
-
-
 
 
 
