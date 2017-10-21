e0ed8b5b2076b8fbaf8433cf723023c2d6eb6330
Leonardo Omaña Cortés <leonardoomanacortes@MacBook-Pro-de-Leonardo.local>
2017-10-21 18:30:15 -0500

    clase 7

--- challenges/OOP/die-class/my_solution.rb
+++ challenges/OOP/die-class/my_solution.rb
@@ -1,5 +1,22 @@
 # Pseudocode
+class Die
+	def initialize(sides)
+		@sides = sides
+			if @sides<1
+				raise ArgumentError.new("Only numbers from one to six")
+			end
+	end
 
+	def sides
+		@sides
+	end
+	def roll
+		rand(@sides) + 1
+	end
+end
+#p die = Die.new(0||6 o cualquier numero) #Esto crea un nuevo Objeto Die con 6 lados
+#p die.sides # returns 6
+#p die.roll # Devuelve un numero al azar entre 1 y 6
 
 
 # 1. Solucion Inicial
@@ -16,3 +33,13 @@
 
 
 ###### DRIVER CODE #########
+
+
+
+
+
+# Clase Dado
+
+## Resumen
+
+
---
+++ challenges/OOP/guessing-game/my_solution.rb
@@ -0,0 +1,10 @@
+class GuessingGame
+	def initialize(answer)
+		@answer = answer	
+	end
+
+	def GuessingGame#guess ()
+	end
+end
+
+ leo = new GuessingGame(answer1......,answern)
