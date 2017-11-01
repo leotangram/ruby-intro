f43b67def30bb50cba6b298ead03d0a23fdbc1e1
juan267 <j.gomez267@gmail.com>
2017-11-01 08:38:51 -0500

    more notes

--- challenges/active-record/2-creating-tables/source/db/migrate/20140901164300_create_dogs.rb
+++ challenges/active-record/2-creating-tables/source/db/migrate/20140901164300_create_dogs.rb
@@ -1,10 +1,11 @@
-class CreateDogs < ActiveRecord::Migration
+class CreateDogs < ActiveRecord::Migration[5.0]
   def change
     create_table :dogs do |t|
       t.string   :name
       t.string   :license
       t.integer  :age
       t.integer  :weight
+      t.integer :owner_id
 
       t.timestamps
     end
