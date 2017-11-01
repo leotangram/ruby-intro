9417980302dd420abbbb9d20887395aa6eb971c6
Joshua Prpich <joshuaprpich@gmail.com>
2017-10-31 21:07:20 -0500

    active record excercises

---
+++ challenges/active-record/3-editing-tables/source/db/migrate/201710312031300_changes_to_dogs.rb
@@ -0,0 +1,8 @@
+class ChangesToDogs < ActiveRecord::Migration
+  def change
+    remove_column :dogs, :weight
+    add_column :dogs, :breed, :string
+  end
+end
+
+
---
+++ challenges/active-record/3-editing-tables/source/db/migrate/201710312046400_changes_to_ratings.rb
@@ -0,0 +1,5 @@
+class ChangesToRatings < ActiveRecord::Migration
+  def change
+    rename_column :ratings, :rater_id, :judge_id
+  end
+end
