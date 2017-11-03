b22e9ba378de2ec951ad7f4990a7b16e11bb10a1
Leonardo Omaña Cortés <leonardoomanacortes@MacBook-Pro-de-Leonardo.local>
2017-11-02 20:59:33 -0500

    Hasta ejercicio 5 de Active Records

--- challenges/active-record/5-validations/source/app/models/person.rb
+++ challenges/active-record/5-validations/source/app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ActiveRecord::Base
+	validates :first_name, :last_name, presence: true
 end
+
+#expected `#<Person id: nil, first_name: nil, last_name: "Smith", 
+#created_at: nil, updated_at: nil>.invalid?` to return true, got false
--- challenges/active-record/5-validations/source/app/models/rating.rb
+++ challenges/active-record/5-validations/source/app/models/rating.rb
@@ -1,2 +1,7 @@
 class Rating < ActiveRecord::Base
+	validates :coolness, numericality: { greater_than: 0, less_than: 11 }
+	validates :cuteness, numericality: { greater_than: 0, less_than: 11 }
+	validates :judge_id, :dog_id, presence: true
+	validates :judge_id, uniqueness: { scope: :dog_id }
 end
+
