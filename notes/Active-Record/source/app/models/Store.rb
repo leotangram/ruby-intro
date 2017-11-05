45658df344040b7bd4cc46f9d25c98bbf456f484
juan267 <j.gomez267@gmail.com>
2017-11-05 10:15:32 -0500

    more notes

--- notes/Active-Record/source/app/models/Store.rb
+++ notes/Active-Record/source/app/models/Store.rb
@@ -1,18 +1,68 @@
 class Store < ActiveRecord::Base
-  belongs_to :manager
+
+  belongs_to :manager, foreign_key: :boss_id
   has_many :products
 
-
-  def products
-    Product.where(store_id: self.id)
-  end
-
-  # belongs_to :manager
-  # es realmente
-  # def manager
-  #   Manager.find_by(id: self.manager_id)
-  # end
-  #
-  # has_many :products
-  # # # es realmente
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Store < ActiveRecord::Base
+#   belongs_to :manager
+#   has_many :products
+
+
+#   def products
+#     Product.where(store_id: self.id)
+#   end
+
+#   # belongs_to :manager
+#   # es realmente
+#   # def manager
+#   #   Manager.find_by(id: self.manager_id)
+#   # end
+#   #
+#   # has_many :products
+#   # # # es realmente
+# end
--- notes/Active-Record/source/db/seeds.rb
+++ notes/Active-Record/source/db/seeds.rb
@@ -5,12 +5,27 @@ product_details = { :barcode    => "AR-23414324",
                    :name     => 'TV',
                    :store_id => 1}
 
+product_details2 = { :barcode    => "PS-23414324",
+                  :quantity => 5,
+                  :name     => 'Playstation',
+                  :store_id => 1}
+
+
 store_details = { :name    => "Kiwi Mart",
+                  :manager_id => 1 }
+store_details2 = { :name    => "Oxo",
                   :manager_id => 1 }
 
 manager_details = { :name    => "Juan"}
 
-
-Product.create!(product_details)
-Store.create!(store_details)
+Product.create!([product_details, product_details2])
+Store.create!([store_details, store_details2])
 Manager.create!(manager_details)
+
+
+
+
+
+
+
+
